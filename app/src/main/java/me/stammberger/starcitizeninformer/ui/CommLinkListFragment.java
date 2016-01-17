4b6d944299624f26efb6dd38bfb0ed266b136973
Stefan Stammberger <some.fusion@gmail.com>
2016-01-17 17:15:53 +0100

    Replaced standard RecyclerView with SuperRecyclerView library

--- app/src/main/java/me/stammberger/starcitizeninformer/ui/CommLinkListFragment.java
+++ app/src/main/java/me/stammberger/starcitizeninformer/ui/CommLinkListFragment.java
@@ -3,17 +3,20 @@ package me.stammberger.starcitizeninformer.ui;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.malinskiy.superrecyclerview.SuperRecyclerView;
 
 import java.util.ArrayList;
 
 import me.stammberger.starcitizeninformer.R;
 import me.stammberger.starcitizeninformer.models.CommLinkModel;
+import timber.log.Timber;
 
 /**
  * A fragment representing a list of Items.
@@ -21,14 +24,14 @@ import me.stammberger.starcitizeninformer.models.CommLinkModel;
  * Activities containing this fragment MUST implement the {@link OnListFragmentInteractionListener}
  * interface.
  */
-public class CommLinkListFragment extends Fragment {
+public class CommLinkListFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
 
     private static final String ARG_COLUMN_COUNT = "column-count";
     private static final String ARG_COMM_LINKS = "comm_links";
 
     private int mColumnCount = 2;
     private OnListFragmentInteractionListener mListener;
-    private RecyclerView mRecyclerView;
+    private SuperRecyclerView mRecyclerView;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -64,9 +67,10 @@ public class CommLinkListFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_comm_link_list, container, false);
 
         // Set the adapter
-        if (view instanceof RecyclerView) {
+        if (view instanceof SuperRecyclerView) {
             Context context = view.getContext();
-            mRecyclerView = (RecyclerView) view;
+            mRecyclerView = (SuperRecyclerView) view;
+            mRecyclerView.setRefreshListener(this);
             if (mColumnCount <= 1) {
                 mRecyclerView.setLayoutManager(new LinearLayoutManager(context));
             } else {
@@ -93,6 +97,15 @@ public class CommLinkListFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    /**
+     * Called when swipe to refresh gesture was accomplished
+     */
+    @Override
+    public void onRefresh() {
+        Timber.d("Refreshing comm links.");
+        mRecyclerView.getSwipeToRefresh().setRefreshing(false);
     }
 
     public interface OnListFragmentInteractionListener {
