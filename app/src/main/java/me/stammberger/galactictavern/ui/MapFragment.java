938a605b359b676ef3472803d74a5a5d14c83bc6
Stefan Stammberger <some.fusion@gmail.com>
2016-05-14 21:24:53 +0200

    Added a loading indicator to the MapFragment

--- app/src/main/java/me/stammberger/galactictavern/ui/MapFragment.java
+++ app/src/main/java/me/stammberger/galactictavern/ui/MapFragment.java
@@ -1,6 +1,7 @@
 package me.stammberger.galactictavern.ui;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,7 +11,6 @@ import com.badlogic.gdx.backends.android.AndroidFragmentApplication;
 
 import me.stammberger.galactictavern.R;
 import me.stammberger.starcitizencompact.map.GtStarMap;
-import timber.log.Timber;
 
 /**
  * Fragment for displaying starmap data.
@@ -18,7 +18,9 @@ import timber.log.Timber;
  * Use the {@link MapFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class MapFragment extends AndroidFragmentApplication {
+public class MapFragment extends AndroidFragmentApplication implements GtStarMap.StatusCallback {
+    public View mLoadingIndicator;
+
     public MapFragment() {
     }
 
@@ -34,28 +36,27 @@ public class MapFragment extends AndroidFragmentApplication {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-
         View v = inflater.inflate(R.layout.fragment_map, container, false);
         FrameLayout starMapView = (FrameLayout) v.findViewById(R.id.starMapFrameLayout);
-        GtStarMap gtStarMap = new GtStarMap(new GtStarMap.StatusCallback() {
-            @Override
-            public void onStartedLoading() {
-                Timber.d("started loading");
-            }
-
-            @Override
-            public void onFinishedLoading() {
-                Timber.d("finished loading");
-            }
-
-            @Override
-            public void onError(String error) {
-
-            }
-        });
+        mLoadingIndicator = v.findViewById(R.id.loadingIndicator);
+        GtStarMap gtStarMap = new GtStarMap(this);
         View view = initializeForView(gtStarMap);
         starMapView.addView(view);
         return v;
+    }
+
+    @Override
+    public void onStartedLoading() {
+    }
+
+    @Override
+    public void onFinishedLoading() {
+        Handler mainHandler = new Handler(getActivity().getMainLooper());
+        Runnable myRunnable = () -> mLoadingIndicator.setVisibility(View.GONE);
+        mainHandler.post(myRunnable);
+    }
+
+    @Override
+    public void onError(String error) {
     }
 }
