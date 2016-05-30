4b91bf37140d82acfe60b2078456a1ca1ae887fd
Stefan Stammberger <some.fusion@gmail.com>
2016-05-30 17:59:06 +0200

    Added leak canary in debug build

--- app/src/debug/java/space/galactictavern/app/GtDebugApplication.java
+++ app/src/debug/java/space/galactictavern/app/GtDebugApplication.java
@@ -1,11 +1,13 @@
 package space.galactictavern.app;
 
 import com.facebook.stetho.Stetho;
+import com.squareup.leakcanary.LeakCanary;
 
 public class GtDebugApplication extends GtApplication {
     @Override
     public void onCreate() {
         super.onCreate();
         Stetho.initializeWithDefaults(this);
+        LeakCanary.install(this);
     }
 }
